93fe7dd4dea997100e850521bb4a0ef9e4da266e
Dim KomAr <dim_kom31@outlook.com>
2019-11-27 10:29:41 +0000

    vso init

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace workspace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name = "Dmitrii";
+
+            Console.WriteLine($"Hello {name}!");
+        }
+    }
+}
